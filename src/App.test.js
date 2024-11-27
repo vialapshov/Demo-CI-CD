fa060a3f64c1ba37d99c34e0a8e3f25884690ac1
vialapshov <via.lapshov@gmail.com>
2024-11-27 11:35:20 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import react-dom/test-utils from 'react-dom';
 import App from './App';
 test('renders learn react link', () => {
   render(<App />);
