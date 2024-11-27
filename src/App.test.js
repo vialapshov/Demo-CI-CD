47ab00ad1f49738529e1ab48054459c3dcc46038
vialapshov <via.lapshov@gmail.com>
2024-11-27 11:31:50 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,9 @@
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import storeConfig from './store/storeConfig';
 import App from './App';
-
-const store = storeConfig();
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
