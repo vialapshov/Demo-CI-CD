9433407e7386c9dce3a9f3dcbabc0b78edfad3f7
vialapshov <via.lapshov@gmail.com>
2024-11-27 13:37:26 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom'
-window.React = React
 import App from './App';
 
 test('renders learn react link', () => {
