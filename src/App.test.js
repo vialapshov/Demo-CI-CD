92349e0e8eea040f0619b5b633cab9810d3a63ac
vialapshov <via.lapshov@gmail.com>
2024-11-27 13:27:53 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
